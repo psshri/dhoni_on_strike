773e3db9549820f79f4a65523852136a8dbf0437
Priyanshu <pshekhar830@gmail.com>
2023-05-13 12:29:55 +0530

    test

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 This application notifies the end user whenever MS Dhoni is out on the pitch to bat!
 
 
-todos:
+### todos:
 - research the capabilities of aws lambda
 - research the capabilities of aws sns
 - research how to send notifications to telegram
@@ -14,12 +14,12 @@ todos:
 - use terraform to deploy infra in aws
 - also use aws SDKs to deploy infra in aws (kiran's requirement)
 
-todos in application code
+### todos in application code
 - use packages in go to make your appliction scalable and reusable, for example a user should be able to enter the player name(s) of his choice and should be notified for his list of player(s). 
 - you should fill the match data also by api endpoints and not manually
 
 
-features:
+### features:
 - should be scalable, i should be able to add users to that notification list
 - notification should be sent via amazon sns and the notification should be sent to telegram
 - store the match details in amazon dynamodb and the code running on aws lambda should be able to fetch those details, make sure security and RBAC is properly delegated
@@ -28,3 +28,8 @@ features:
 - application code has multiple packages, so as to make the code reusable, 
 - user notification logic will be mentioned in the application code but all the tasks (storing user data, etc) related to notification will be handled by sns
 - user should also be notified via a phone call
+
+
+
+### imp links
+- https://github.com/mskian/cricket-cli
--- src/main.go
+++ src/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -20,7 +22,59 @@ func main() {
 	defer res.Body.Close()
 	body, _ := io.ReadAll(res.Body)
 
-	fmt.Println(res)
-	fmt.Println(string(body))
+	// fmt.Println(res)
+	// fmt.Println(string(body))
+
+	// create a file output.txt
+	file, err := os.Create("output.json")
+	if err != nil {
+		fmt.Println("Error creating file: ", err)
+		return
+	}
+	defer file.Close()
+
+	// // write body to the file output.txt
+	// _, err = file.Write(body)
+	// if err != nil {
+	// 	fmt.Println("Error writing to file: ", err)
+	// 	return
+	// }
+
+	// fmt.Println("Output written to file successfully!")
+
+	// explore the data type
+	// body_string := string(body)
+	var data_json interface{}
+	err2 := json.Unmarshal(body, &data_json)
+	if err2 != nil {
+		fmt.Println("Error unmarshaling body JSON: ", err2)
+		return
+	}
+
+	formattedData, _ := json.MarshalIndent(data_json, "", "    ")
+	// fmt.Println(string(formattedData))
+
+	// writing formattedData to file
+	_, err = file.Write(formattedData)
+	if err != nil {
+		fmt.Println("Error writing to file: ", err)
+		return
+	}
+
+	fmt.Println("Output written to file successfully!")
+
+	var data_map map[string]interface{}
+	err1 := json.Unmarshal(formattedData, &data_map)
+	if err1 != nil {
+		fmt.Println("Error unmarshaling formattedData JSON: ", err1)
+		return
+	}
+
+	for key := range data_map {
+		fmt.Println(key)
+	}
+
+	meta := data_map.(map[string]interface{})["meta"]
+	fmt.Println(meta)
 
 }
