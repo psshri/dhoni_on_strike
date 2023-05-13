9124ac6fb0b155177da06acb4393398bf45aa124
Priyanshu <pshekhar830@gmail.com>
2023-05-13 18:34:09 +0530

    fixtures data fetched in json format

--- src/main.go
+++ src/main.go
@@ -25,8 +25,8 @@ func hitAPI(url string) []uint8 {
 
 func main() {
 
-	// fixtures
-	url_fixtures := "https://cricket-live-data.p.rapidapi.com/fixtures"
+	// IPL fixtures
+	url_fixtures := "https://cricket-live-data.p.rapidapi.com/fixtures-by-series/1430"
 	body_fixtures := hitAPI(url_fixtures)
 
 	// create a file to store the data
@@ -53,75 +53,4 @@ func main() {
 		return
 	}
 	fmt.Println("Output written to file successfully!")
-
-	// url := "https://cricket-live-data.p.rapidapi.com/series"
-
-	// body := hitAPI(url)
-
-	// find out the data type of body variable
-	// fmt.Println(reflect.TypeOf(body))
-
-	// fmt.Println(res)
-	// fmt.Println(string(body))
-
-	// create a file output.txt
-	// file, err := os.Create("output.json")
-	// if err != nil {
-	// 	fmt.Println("Error creating file: ", err)
-	// 	return
-	// }
-	// defer file.Close()
-
-	// // write body to the file output.txt
-	// _, err = file.Write(body)
-	// if err != nil {
-	// 	fmt.Println("Error writing to file: ", err)
-	// 	return
-	// }
-
-	// fmt.Println("Output written to file successfully!")
-
-	// explore the data type
-	// body_string := string(body)
-	// var data_json interface{}
-	// err2 := json.Unmarshal(body, &data_json)
-	// if err2 != nil {
-	// 	fmt.Println("Error unmarshaling body JSON: ", err2)
-	// 	return
-	// }
-
-	// formattedData, _ := json.MarshalIndent(data_json, "", "    ")
-	// // fmt.Println(string(formattedData))
-
-	// // writing formattedData to file
-	// _, err = file.Write(formattedData)
-	// if err != nil {
-	// 	fmt.Println("Error writing to file: ", err)
-	// 	return
-	// }
-
-	// // fmt.Println("Output written to file successfully!")
-
-	// var data_map map[string]interface{}
-	// err1 := json.Unmarshal(formattedData, &data_map)
-	// if err1 != nil {
-	// 	fmt.Println("Error unmarshaling formattedData JSON: ", err1)
-	// 	return
-	// }
-
-	// for key := range data_map {
-	// 	fmt.Println(key)
-	// }
-
-	// meta := data_map.(map[string]interface{})["meta"]
-	// fmt.Println(meta)
-
-	// fmt.Println(data_map["meta"])
-
-	// // used to find the type of data in golang
-	// fmt.Println(reflect.TypeOf(data_map["meta"].(map[string]interface{})))
-
-	// meta := data_map["meta"].(map[string]interface{})
-	// fmt.Println(meta)
-
 }
