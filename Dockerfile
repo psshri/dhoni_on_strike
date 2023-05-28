439741a27cc25efa420bf8d6c7bc04f968d87b9e
Priyanshu <pshekhar830@gmail.com>
2023-05-28 13:35:05 +0530

    python code completed

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,19 @@
-# use an official python runtime as the base image, python:3.9-alpine is lightweight will not increase the size of the image a lot
-FROM python:3.9-alpine
+FROM public.ecr.aws/lambda/python:3.10
 
 # set the working directory inside the container
-WORKDIR /app
+# WORKDIR /app
 
 # copy the requirements file to the container
 COPY requirements.txt .
 
 # install the python dependencies
-RUN pip install --no-cache-dir -r requirements.txt
+RUN pip3 install -r requirements.txt --target "${LAMBDA_TASK_ROOT}"
 
 # copy the relevant files from src to the container
-COPY src/config/info.json config/
-COPY src/checkLiveScore/checkLiveScore.py checkLiveScore/
-COPY src/checkSchedule/checkSchedule.py checkSchedule/
-COPY src/main.py .
+# COPY src/config/info.json config/
+COPY src/checkLiveScore/checkLiveScore.py ${LAMBDA_TASK_ROOT}/checkLiveScore/
+COPY src/checkSchedule/checkSchedule.py ${LAMBDA_TASK_ROOT}/checkSchedule/
+COPY src/main.py ${LAMBDA_TASK_ROOT}
 
 # set the default values for environment variables
 ENV PLAYER_ID=84255
@@ -25,4 +24,4 @@ ENV INTERVAL=2
 ENV SECRET_NAME="dhoni_on_strike"
 
 # specify the command to run your application
-CMD ["python", "main.py"]
+CMD ["main.handler"]
--- src/main.py
+++ src/main.py
@@ -12,6 +12,11 @@ from botocore.exceptions import ClientError
 from checkSchedule.checkSchedule import get_schedule, evaluate_schedule
 from checkLiveScore.checkLiveScore import get_live_score, is_batting
 
+# global variables ##################################################
+
+counter = {'value': None}
+alerted = {'value': None}
+
 # helper functions ##################################################
 
 def func_today_string():
@@ -52,46 +57,21 @@ def get_secret(secret_name):
 
     return telegram_chat_id, telegram_bot_token, rapidAPI_api_key
 
-# constants #########################################################
-
-fixtures_url = "https://cricket-live-data.p.rapidapi.com/fixtures-by-date/"
-live_score_url = "https://cricket-live-data.p.rapidapi.com/match/"
-X_RapidAPI_Host = "cricket-live-data.p.rapidapi.com"
-
-today_string = func_today_string()
-# today_string = "2023-05-19"
-ipl_series_id = 1430
-
-team_id = os.environ.get('TEAM_ID')
-team_id = int(team_id)
-team_name = os.environ.get('TEAM_NAME')
-player_id = os.environ.get('PLAYER_ID')
-player_id = int(player_id)
-player_name = os.environ.get('PLAYER_NAME')
-interval = os.environ.get('INTERVAL')
-interval = int(interval)
-
-secret_name = os.environ.get('SECRET_NAME')
-telegram_chat_id, telegram_bot_token, rapidAPI_api_key = get_secret(secret_name)
-
-fixture_data_path = "/tmp/fixtures.json"
-live_score_data_path = "/tmp/live_score.json"
-
 # functions ########################################################
 
-# counter = None
-counter = {'value': None}
-
-def update_counter():
+def update_counter(live_score_data_path, player_id):
     global counter
     counter['value'] = is_batting(live_score_data_path, player_id)
 
-def print_status(counter):
+def print_status(counter, player_name, telegram_bot_token, telegram_chat_id, team_name):
     print("\n")
     if counter['value'] == 1:
         print(player_name + " is on strike!")
         message = player_name + ' is on strike\\!'
         telegram_bot_send_text(message, telegram_bot_token, telegram_chat_id)
+        global alerted
+        alerted['value'] = 1
+
 
     else:
         print(team_name +"'" + " match is today, " + player_name + " is yet to bat!")
@@ -101,27 +81,69 @@ def print_status(counter):
 
 # function calls ###################################################
 
-get_schedule(fixtures_url, X_RapidAPI_Host, rapidAPI_api_key, today_string, fixture_data_path)
+def main():
+    fixtures_url = "https://cricket-live-data.p.rapidapi.com/fixtures-by-date/"
+    live_score_url = "https://cricket-live-data.p.rapidapi.com/match/"
+    X_RapidAPI_Host = "cricket-live-data.p.rapidapi.com"
 
-match_today, match_time_730, match_time_330, match_info = evaluate_schedule(fixture_data_path, ipl_series_id, team_id, today_string)
+    today_string = func_today_string()
+    # today_string = "2023-05-19"
+    ipl_series_id = 1430
 
-if match_today == 1:
+    team_id = os.environ.get('TEAM_ID')
+    team_id = int(team_id)
+    team_name = os.environ.get('TEAM_NAME')
+    player_id = os.environ.get('PLAYER_ID')
+    player_id = int(player_id)
+    player_name = os.environ.get('PLAYER_NAME')
+    interval = os.environ.get('INTERVAL')
+    interval = int(interval)
 
-    schedule.every(interval).seconds.do(get_live_score, match_info=match_info,
-                                  url=live_score_url,
-                                  rapidAPI_api_key=rapidAPI_api_key,
-                                  X_RapidAPI_Host=X_RapidAPI_Host,
-                                  live_score_data_path=live_score_data_path)
+    secret_name = os.environ.get('SECRET_NAME')
+    telegram_chat_id, telegram_bot_token, rapidAPI_api_key = get_secret(secret_name)
 
-    schedule.every(interval).seconds.do(update_counter)
+    fixture_data_path = "/tmp/fixtures.json"
+    live_score_data_path = "/tmp/live_score.json"
 
-    schedule.every(interval).seconds.do(print_status, counter=counter)
+    get_schedule(fixtures_url, X_RapidAPI_Host, rapidAPI_api_key, today_string, fixture_data_path)
+    match_today, match_time_730, match_time_330, match_info = evaluate_schedule(fixture_data_path, ipl_series_id, team_id, today_string)
 
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+    if match_today == 1:
 
-else:
-    print("No " + team_name + "' match today!")
-    message = 'No ' + team_name + ' match today\\!'
-    telegram_bot_send_text(message, telegram_bot_token, telegram_chat_id)
+        print(team_name + "' match is today!")
+        message = team_name + ' match is today\\!'
+        telegram_bot_send_text(message, telegram_bot_token, telegram_chat_id)
+
+        schedule.every(interval).seconds.do(get_live_score, match_info=match_info,
+                                    url=live_score_url,
+                                    rapidAPI_api_key=rapidAPI_api_key,
+                                    X_RapidAPI_Host=X_RapidAPI_Host,
+                                    live_score_data_path=live_score_data_path)
+
+        schedule.every(interval).seconds.do(update_counter, 
+                                            live_score_data_path=live_score_data_path, 
+                                            player_id=player_id)
+
+        schedule.every(interval).seconds.do(print_status, 
+                                            counter=counter, 
+                                            player_name=player_name, 
+                                            telegram_bot_token=telegram_bot_token, 
+                                            telegram_chat_id=telegram_chat_id, 
+                                            team_name=team_name)
+
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
+            global alerted
+            if alerted['value'] == 1:
+                break
+
+    else:
+        print("No " + team_name + "' match today!")
+        message = 'No ' + team_name + ' match today\\!'
+        telegram_bot_send_text(message, telegram_bot_token, telegram_chat_id)
+
+    return 'done!'
+
+def handler(event, context):
+    return main()
